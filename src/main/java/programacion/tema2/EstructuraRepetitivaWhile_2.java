98616e0bff0c49951a8011be6672d0abf6535af4
BLL75 <bluquelafuentegmail.com>
2025-10-25 23:41:02 +0100

    While 2

---
+++ src/main/java/programacion/tema2/EstructuraRepetitivaWhile_2.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package programacion.tema2;
+import java.util.Scanner;
+/**
+ *
+ * @author Bernar
+ */
+public class EstructuraRepetitivaWhile_2 {
+    
+    public static void main(String[] args){
+        
+        int num1 = 1;
+        int num2 = 7;
+        
+        while (num1 <= num2){
+            System.out.println(num1 + " * " + num2 + " = " + (num1 * num2) + "\n"  );
+            num1++;
+        }
+    }
+    
+}
